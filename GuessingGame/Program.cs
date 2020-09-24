0f12313a5e578497d871828958916b4bbde795d3
Opilane TTHK <opilane@tthk.ee>
2020-09-24 12:31:12 +0300

    first commit

---
+++ GuessingGame/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace GuessingGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int randomNumber = rnd.Next(1, 11);
+            Console.WriteLine("What is my number?");
+            int UserGuess = Int32.Parse(Console.ReadLine());
+            bool correctGuess = false;
+
+            while(!correctGuess)
+            {
+                if(randomNumber == UserGuess)
+                {
+                    Console.WriteLine("You won!");
+                    correctGuess = true;
+                } else
+                {
+                    if (randomNumber > UserGuess)
+                    { 
+
+                        Console.WriteLine("My number is bigger.");
+                        Console.WriteLine("What is my number?");
+                        UserGuess = Int32.Parse(Console.ReadLine());
+                    }
+                    else
+                    {
+                        Console.WriteLine("My number is smaller.");
+                        Console.WriteLine("What is my number?");
+                        UserGuess = Int32.Parse(Console.ReadLine());
+                    }
+
+
+                }
+            }
+        }
+    }
+}
---
+++ ThreeTries/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ThreeTries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int randomnumber = rnd.Next(1, 11);
+            int userInput;
+            int counter = 0;
+
+            while(counter < 3)
+            {
+                Console.WriteLine("What is my number?");
+                userInput = Int32.Parse(Console.ReadLine());
+                if(randomnumber == userInput)
+                {
+                    Console.WriteLine("Congratulations, you won!");
+                    break;
+                } else
+                {
+                    Console.WriteLine("Try again!");
+                    counter = counter + 1;
+                    Console.WriteLine($"You have tried {counter} time");
+
+                }
+               
+                
+
+            }
+
+
+
+
+
+        }
+    }
+}
